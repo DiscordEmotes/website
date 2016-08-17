768d9b264925fec92bc526e8c7a8fa50572bdf88
Rapptz <rapptz@gmail.com>
2016-08-17 17:09:32 -0400

    Basic OAuth2 flow implementation.

--- README.md
+++ README.md
@@ -4,9 +4,21 @@ Maybe.
 
 Probably not.
 
-Requirements:
+## Requirements
 
 - Some undetermined python version
 - Flask
 
 Just do `pip install -r requirements.txt`.
+
+## Running
+
+Make a `config.py` file in the root directory with the following variables:
+
+```py
+OAUTH2_CLIENT_ID = 'my client ID'
+OAUTH2_SECRET_KEY = 'my app secret'
+OAUTH2_REDIRECT_URI = 'http://localhost:5000/callback'
+
+SECRET_KEY = OAUTH2_SECRET_KEY
+```
--- website/views.py
+++ website/views.py
@@ -1,7 +1,30 @@
-from website import app
-from flask import render_template
+from website import app, make_session, get_current_user, DISCORD_AUTH_BASE_URL, DISCORD_TOKEN_URL
+from flask import render_template, session, request, redirect, url_for
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    user = get_current_user()
+    return render_template('index.html', user=user)
+
+@app.route('/login')
+def login():
+    with make_session() as discord:
+        url, state = discord.authorization_url(DISCORD_AUTH_BASE_URL)
+        session['oauth2_state'] = state
+        return redirect(url)
+
+@app.route('/callback')
+def callback():
+    state = session.get('oauth2_state')
+    if not state and request.values.get('error'):
+        return redirect(url_for('index'))
+
+    with make_session(state=state) as discord:
+        token = discord.fetch_token(DISCORD_TOKEN_URL,
+                                    client_secret=app.config['OAUTH2_SECRET_KEY'],
+                                    authorization_response=request.url)
+
+        session['oauth2_token'] = token
+        session.permanent = True
+        return redirect(url_for('index'))
